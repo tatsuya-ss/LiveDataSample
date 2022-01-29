cd733b34ce5b13ca41c2d3762992c60ab2c59c78
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-29 16:03:33 +0900

    データバインディングのリファクタリング

--- app/src/main/java/com/example/livedatasample/MainActivity.kt
+++ app/src/main/java/com/example/livedatasample/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.livedatasample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.livedatasample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val numberViewModel = NumberViewModel()
+    private val numberViewModel: NumberViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +23,10 @@ class MainActivity : AppCompatActivity() {
             this, R.layout.activity_main
         ).apply {
             // これでLiveDataが正しく動く
+            // https://developer.android.com/reference/android/databinding/ViewDataBinding
             setLifecycleOwner(this@MainActivity)
         }
-        binding.numberViewModel = this.numberViewModel
+        binding.numberViewModel = numberViewModel
     }
 
 }
